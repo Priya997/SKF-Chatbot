5a0c932edfa7d93901a4ac4a2423cea81f32f92c
Priya997 <priyankajain997@gmail.com>
2018-06-07 13:21:58 -0700

    improvements1

--- Basic_Approach/test.py
+++ Basic_Approach/test.py
@@ -4,22 +4,25 @@ import json
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 
+def answer(question):
+	
+	intent=intent_classifier.predict(question)
+	entity=entity_classifier.entity(question)
+
+
+	read_file = open("datasets/desc_sol.json")
+	data = json.load(read_file)
+
+	ite=data['items']
+	if type(entity)==str:
+		for d in ite:
+			if entity==d['title'].lower():
+				print(d[intent])
+	else:
+		print(entity)
+		ques=input("enter your choice ")
+		answer(ques)	
 
 question=input("Enter Question ")
-intent=intent_classifier.predict(question)
-entity=entity_classifier.entity(question)
-
-
-read_file = open("datasets/desc_sol.json")
-data = json.load(read_file)
-
-ite=data['items']
-if type(entity)==str:
-	for d in ite:
-		if entity==d['title'].lower():
-        		print(d[intent])
-else:
-	print(entity)
-	
-	
+answer(question)
 
